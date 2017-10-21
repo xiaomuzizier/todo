d013cdd1cdce5d66bf4edfa27d8cb3bcacad6b2c
xiaomuzizi <13051391963@163.com>
2017-10-21 16:02:25 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <CompletedTodos></CompletedTodos>
+    <CurrentTodos></CurrentTodos>
+   <GetTodo></GetTodo>
+  </div>
+</template>
+
+<script>
+  import GetTodo from './components/GetTodo.vue'
+  import CurrentTodos from './components/CurrentTodos.vue'
+  import CompletedTodos from './components/CompletedTodos.vue'
+export default {
+   components:{
+     GetTodo,
+     CurrentTodos,
+     CompletedTodos
+   }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
